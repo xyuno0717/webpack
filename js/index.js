91771546e82ba7549cf887eba5c8f3df129e29fb
nishimaru <xyumu56@gmail.com>
2020-02-07 19:40:04 +0900

    no message

--- js/index.js
+++ js/index.js
@@ -1,10 +1,5 @@
-
+import './test.scss';
 import * as util from './utility.js'
-
 ///////////////////////////////////////////////////////////////////////
 
-// テスト1
-// util.modalOpen();
-
-// テスト2
 document.getElementById("js_open").addEventListener("click", util.modalOpen, false);
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,46 @@
 module.exports = {
     // メインとなるJavaScriptファイル（エントリーポイント）
     entry: "./js/index.js",
-    mode: "production",
+    mode: "development",
     // ファイルの出力設定
     output: {
         //  出力ファイルのディレクトリ名
         path: `${__dirname}/dist`,
         // 出力ファイル名
-        filename: "main.js"
-    }
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [    
+          // SASS
+          {
+            test: /\.scss/,
+            use: [
+                // linkタグに出力する機能
+                "style-loader",
+                // CSSをバンドルするための機能
+                {
+                  loader: "css-loader",
+                  options: {
+                    // オプションでCSS内のurl()メソッドの取り込みを禁止する
+                    url: false,
+                    // ソースマップの利用有無
+                    sourceMap: enabledSourceMap,
+      
+                    // 0 => no loaders (default);
+                    // 1 => postcss-loader;
+                    // 2 => postcss-loader, sass-loader
+                    importLoaders: 2
+                  }
+                },
+                {
+                  loader: "sass-loader",
+                  options: {
+                    // ソースマップの利用有無
+                    sourceMap: enabledSourceMap
+                  }
+                }
+              ]
+          }
+        ]
+    },
 };
